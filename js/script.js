3cacc2897fd48711e2b350847c45e24ddae5da22
Andrea <andrea3aprile99@gmail.com>
2021-11-17 22:24:33 +0100

    indentazione

--- js/script.js
+++ js/script.js
@@ -1,42 +1,43 @@
 
-  var app = new Vue({
-    el: '#app',
-    data : {
-      videoRecharge:null,
-      videorandom :null,
-       x:null,
-        info: null,
-        loading: true,
-        errored: false,
-      images :[]
-    },
-    methods:{
-      
-    },
+var app = new Vue({
+  el: '#app',
+  
+  data : {
+    videoRecharge:null,
+    videorandom :null,
+    x:null,
+    info: null,
+    loading: true,
+    errored: false,
+    images :[]
+  },
 
-    mounted: function() {
-      x = Math.floor((Math.random() * 10) + 1);
-      
-       var API_KEY = 'https://pixabay.com/api/videos/?key=23204036-69c0429327c4664dc706ccca6&q=yellow+flowers&profile_id='+ x;
-      axios
-      .get(API_KEY)
-      .then((result)=>{
-          this.images = result.data.hits;
-          
-        
-        this.videorandom = this.images[x];
-        // console.log(x)
-        // console.log(this.videorandom.videos.large.url)
-        this.videoRecharge = this.videorandom.videos.large.url
-        })
-       
-        .catch(error => {
-          console.log(error)
-          this.errored = true
-        })
-        .finally(() => this.loading = false)
-       
-         
-    }
-  })
+  methods:{
 
+  },
+
+  mounted: function() {
+    x = Math.floor((Math.random() * 10) + 1);
+
+    var API_KEY = 'https://pixabay.com/api/videos/?key=23204036-69c0429327c4664dc706ccca6&q=yellow+flowers&profile_id='+ x;
+    axios
+
+    .get(API_KEY)
+    .then((result)=>{
+      this.images = result.data.hits;
+      this.videorandom = this.images[x];
+      // console.log(x)
+      // console.log(this.videorandom.videos.large.url)
+      this.videoRecharge = this.videorandom.videos.large.url
+    })
+
+    .catch(error => {
+      console.log(error)
+      this.errored = true
+    })
+
+    .finally(() => this.loading = false)
+
+
+  }
+})
